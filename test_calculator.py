a169307b68953a25e1fa92aeaf8f73b5cab55103
hussanmunir <hussanmunir@users.noreply.github.com>
2025-12-16 17:29:16 +0100

    Fixed test case

--- test_calculator.py
+++ test_calculator.py
@@ -13,7 +13,7 @@ class TestCalculator:
         assert self.calculator.add(-1, 1) == 0
 
     def test_subtract(self):
-        assert self.calculator.subtract(5, 3) == 1
+        assert self.calculator.subtract(5, 3) == 2
     
     '''
     def test_subtract_negative(self):
